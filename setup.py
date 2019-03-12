b863a8e15444538b0644d9e22e8591232280e478
komuw <komuw05@gmail.com>
2019-03-12 16:15:30 +0300

    aa

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["botocore>=1.12.0,<1.13.0"],
+    install_requires=["wiji>=0.1.0,<1.0.0", "botocore>=1.12.0,<1.13.0"],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
